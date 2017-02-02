eaf9dfa60df6de922dce83c812aab84e4099498e
austin.drenski <austin.drenski@gmail.com>
2017-02-02 16:39:43 -0500

    Added xml documentation and attributes to Previous

--- AD.Xml/src/Previous.cs
+++ AD.Xml/src/Previous.cs
@@ -3,6 +3,9 @@ using JetBrains.Annotations;
 
 namespace AD.Xml
 {
+    /// <summary>
+    /// Extension methods to navigate between ordered nodes.
+    /// </summary>
     [PublicAPI]
     public static class PreviousExtensions
     {
@@ -12,9 +15,44 @@ namespace AD.Xml
         /// <param name="element">The source element.</param>
         /// <returns>The previous sibling <see cref="XElement"/>.</returns>
         [CanBeNull]
-        public static XElement Previous(this XElement element)
+        [Pure]
+        public static XElement Previous([NotNull] this XElement element)
         {
             return element.PreviousNode as XElement;
+        }
+
+        /// <summary>
+        /// Returns the next node (in document order) of the given name as an <see cref="XElement"/> or null.
+        /// </summary>
+        /// <param name="element">The source element.</param>
+        /// <param name="name">The name of the next element to find.</param>
+        /// <returns>The next sibling <see cref="XElement"/>.</returns>
+        [CanBeNull]
+        [Pure]
+        public static XElement Previous([NotNull] this XElement element, [NotNull] XName name)
+        {
+            XElement host = element;
+            while (host.Parent != null)
+            {
+                host = host.Parent;
+            }
+            bool check = false;
+            foreach (XElement item in host.DescendantsAndSelf().InDocumentOrder())
+            {
+                if (item.Next() == element)
+                {
+                    check = true;
+                }
+                if (check)
+                {
+                    continue;
+                }
+                if (item.Name == name)
+                {
+                    return item;
+                }
+            }
+            return null;
         }
     }
 }
