48e32d0cc64b55ec6d9770375adb76370ff7b7ab
austin.drenski <austin.drenski@gmail.com>
2017-02-02 16:24:09 -0500

    Added xml documentation and attributes to Detach

--- AD.Xml/src/Concat.cs
+++ AD.Xml/src/Concat.cs
@@ -15,7 +15,6 @@ namespace AD.Xml
         /// <param name="strings">The source enumerable.</param>
         /// <param name="join">The string that joins the items in the enumerable.</param>
         /// <returns>The string formed by concatenating the strings of the enumerable.</returns>
-        /// <exception cref="System.ArgumentNullException"/>
         [CanBeNull]
         [Pure]
         public static string Concat([NotNull] this IEnumerable<string> strings, string join = null)
--- AD.Xml/src/Detach.cs
+++ AD.Xml/src/Detach.cs
@@ -5,12 +5,55 @@ using JetBrains.Annotations;
 
 namespace AD.Xml
 {
+    /// <summary>
+    /// Detaches each item in the enumerable from its parent node.
+    /// </summary>
     [PublicAPI]
     public static class DetachExtensions
     {
-        public static IEnumerable<XElement> Detach(this IEnumerable<XElement> elements)
+        /// <summary>
+        /// Detaches each item in the enumerable from its parent node.
+        /// </summary>
+        /// <param name="elements">The elements to detach from their parent nodes.</param>
+        /// <returns>An <see cref="IEnumerable{XElement}"/> whose elements are the result of invoking the transform function on each element of source.</returns>
+        /// <exception cref="System.ArgumentNullException"/>
+        /// <exception cref="System.InvalidOperationException"/>
+        [ItemNotNull]
+        [NotNull]
+        [Pure]
+        public static IEnumerable<XElement> Detach([NotNull][ItemNotNull] this IEnumerable<XElement> elements)
         {
-            return elements.Select(x => { x.Remove(); return x; });
+            return elements.Select(x =>
+            {
+                if (x.Parent != null)
+                {
+                    x.Remove();
+                }
+                return x;
+            });
+        }
+
+        /// <summary>
+        /// Detaches each item in the enumerable from its parent node.
+        /// </summary>
+        /// <param name="elements">The elements to detach from their parent nodes.</param>
+        /// <returns>A <see cref="ParallelQuery{XElement}"/> whose elements are the result of invoking the transform function on each element of source.</returns>
+        /// <exception cref="System.AggregateException"/>
+        /// <exception cref="System.ArgumentNullException"/>
+        /// <exception cref="System.InvalidOperationException"/>
+        [ItemNotNull]
+        [NotNull]
+        [Pure]
+        public static ParallelQuery<XElement> Detach([NotNull][ItemNotNull] this ParallelQuery<XElement> elements)
+        {
+            return elements.Select(x =>
+            {
+                if (x.Parent != null)
+                {
+                    x.Remove();
+                }
+                return x;
+            });
         }
     }
 }
