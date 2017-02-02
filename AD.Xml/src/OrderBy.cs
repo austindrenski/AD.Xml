ae6df7815418d489e9719200cd1df14ebe81d519
austin.drenski <austin.drenski@gmail.com>
2017-02-02 16:34:51 -0500

    Added xml documentation and attributes to OrderBy

--- AD.Xml/src/OrderBy.cs
+++ AD.Xml/src/OrderBy.cs
@@ -7,16 +7,6 @@ using JetBrains.Annotations;
 namespace AD.Xml
 {
     /// <summary>
-    /// Enum describing the direction of a sort.
-    /// </summary>
-    public enum SortOrderType
-    {
-        Ascending,
-        Descending,
-        DoNotUse
-    }
-
-    /// <summary>
     /// Extension methods to sort the elements of the root element in an XDocument.
     /// </summary>
     [PublicAPI]
@@ -28,24 +18,35 @@ namespace AD.Xml
         /// <param name="document">The XDocument to sort.</param>
         /// <param name="sortOrder">An IDictionary of XNames and SortOrderTypes.</param>
         /// <returns>A sorted XDocument.</returns>
-        public static XDocument OrderBy(this XDocument document, IDictionary<XName, SortOrderType> sortOrder)
+        /// <exception cref="ArgumentNullException"/>
+        [NotNull]
+        [Pure]
+        public static XDocument OrderBy(this XDocument document, [NotNull] IDictionary<XName, SortOrderType> sortOrder)
         {
+            if (!sortOrder.Any())
+            {
+                return document;
+            }
             IOrderedEnumerable<XElement> orderedDocument;
             switch (sortOrder.First().Value)
             {
                 case SortOrderType.Ascending:
                 {
-                    orderedDocument = document.Elements()
+                    orderedDocument = document.Root?
                                               .Elements()
                                               .OrderBy(x => x.Element(sortOrder.First().Key)?.Value);
                     break;
                 }
                 case SortOrderType.Descending:
                 {
-                    orderedDocument = document.Elements()
+                    orderedDocument = document.Root?
                                               .Elements()
                                               .OrderByDescending(x => x.Element(sortOrder.First().Key)?.Value);
                     break;
+                }
+                case SortOrderType.DoNotUse:
+                {
+                    throw new NotImplementedException();
                 }
                 default:
                 {
@@ -58,13 +59,17 @@ namespace AD.Xml
                 {
                     case SortOrderType.Ascending:
                     {
-                        orderedDocument = orderedDocument.ThenBy(x => x.Element(kvp.Key)?.Value);
+                        orderedDocument = orderedDocument?.ThenBy(x => x.Element(kvp.Key)?.Value);
                         break;
                     }
                     case SortOrderType.Descending:
                     {
-                        orderedDocument = orderedDocument.ThenByDescending(x => x.Element(kvp.Key)?.Value);
+                        orderedDocument = orderedDocument?.ThenByDescending(x => x.Element(kvp.Key)?.Value);
                         break;
+                    }
+                    case SortOrderType.DoNotUse:
+                    {
+                        throw new NotImplementedException();
                     }
                     default:
                     {
@@ -81,17 +86,20 @@ namespace AD.Xml
         /// <param name="document">The XDocument to sort.</param>
         /// <param name="sortStrings">An enumerable collection of names and sort directions. E.g. "name|asc".</param>
         /// <returns>A sorted XDocument.</returns>
-        public static XDocument OrderBy(this XDocument document, IEnumerable<string> sortStrings)
+        /// <exception cref="ArgumentException"/>
+        /// <exception cref="ArgumentNullException"/>
+        [NotNull]
+        [Pure]
+        public static XDocument OrderBy([NotNull] this XDocument document, [NotNull] IEnumerable<string> sortStrings)
         {
-            if (sortStrings == null)
-            {
-                return document;
-            }
-            IEnumerable<string> sortStringArrays = sortStrings as string[] ?? sortStrings.ToArray();
+            IEnumerable<string> sortStringArrays = 
+                sortStrings as string[] ?? sortStrings.ToArray();
+
             if (!sortStringArrays.Any())
             {
                 return document;
             }
+
             return document.OrderBy(
                                     sortStringArrays.SkipWhile(string.IsNullOrEmpty)
                                                     .Select(x => x.Contains('|') ? x : $"{x}|asc")
@@ -100,5 +108,26 @@ namespace AD.Xml
                                                                   x => x[1].ToLower().StartsWith("asc") ? SortOrderType.Ascending 
                                                                                                         : SortOrderType.Descending));
         }
+    }
+
+    /// <summary>
+    /// Enum describing the direction of a sort.
+    /// </summary>
+    public enum SortOrderType
+    {
+        /// <summary>
+        /// Sorted in ascending order.
+        /// </summary>
+        Ascending,
+
+        /// <summary>
+        /// Sorted in descending order.
+        /// </summary>
+        Descending,
+
+        /// <summary>
+        /// Not used in sort.
+        /// </summary>
+        DoNotUse
     }
 }
