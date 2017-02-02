83b1b33adbe5de00ca824d65b5fa909e8f18c40e
austin.drenski <austin.drenski@gmail.com>
2017-02-02 16:17:34 -0500

    Added xml documentation and attributes to Concat

--- AD.Xml/src/Concat.cs
+++ AD.Xml/src/Concat.cs
@@ -3,10 +3,22 @@ using JetBrains.Annotations;
 
 namespace AD.Xml
 {
+    /// <summary>
+    /// Concatenates items of enumerable collections.
+    /// </summary>
     [PublicAPI]
     public static class ConcatExtensions
     {
-        public static string Concat(this IEnumerable<string> strings, string join = null)
+        /// <summary>
+        /// Concatenates the strings of the enumerable optionally inserting a string separator.
+        /// </summary>
+        /// <param name="strings">The source enumerable.</param>
+        /// <param name="join">The string that joins the items in the enumerable.</param>
+        /// <returns>The string formed by concatenating the strings of the enumerable.</returns>
+        /// <exception cref="System.ArgumentNullException"/>
+        [CanBeNull]
+        [Pure]
+        public static string Concat([NotNull] this IEnumerable<string> strings, string join = null)
         {
             return string.Join(join, strings);
         }
